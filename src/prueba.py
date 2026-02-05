f88493fc3f350f42bb2bd5ed562b79eebedd0f74
= <=>
2026-02-04 18:59:24 -0600

    Auto 2026-02-04 18:59

---
+++ src/prueba.py
@@ -0,0 +1,5 @@
+a = [[ 0 for _ in range(5)] for _ in range(3)]  # 3 X 5
+b = [[0]*5 for _ in range(3)]  # 3 X 5
+c = [[0]*5]*3  # 3 X 5
+
+
--- src/u1/cartas.py
+++ src/u1/cartas.py
@@ -1,2 +1,41 @@
-        String[] valor = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
-        String[] figura = {"C♥","D♦","T♣","P♠"};
+#String[] valor = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+#String[] figura = {"C♥","D♦","T♣","P♠"};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import random
+
+
+def triangular1(n):
+    for i in [[1 if i<=j else 0 for i in range(n)] for j in range(n)]: # [::-1]
+        print(i)
+
+def triangular2(n):
+    for i in [[1 if i<=j else 0 for i in range(n)] for j in range(n)] [::-1]:
+        print(i)
+
+triangular1(5)
+print()
+triangular2(5)
+
+matrix = [[random.randint(0,20) for i in range(5)] for j in range(5)]
+print(matrix)
+
+for i in range(5):
+    for j in range(5):
+        print(matrix[i][j], end="\t")
+    print()
+print()    
+print(matrix[::-1])
